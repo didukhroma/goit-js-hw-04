9d18ea2330363290c096b959dd0a5cc82486c582
didukhroma <70459354+didukhroma@users.noreply.github.com>
2020-12-17 21:09:32 +0200

    task-2-bind

--- js/task-2.js
+++ js/task-2.js
@@ -1,22 +1,54 @@
 // Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+//----------I-variant--------------------------
+// const inventory = {
+//   items: ['Knife', 'Gas mask'],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
 
+//     inventory.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
+
+//     inventory.items = inventory.items.filter(item => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryAction = function (itemName, action) {
+//   console.log(`Invoking action on ${itemName}`);
+//   action(itemName);
+// };
+
+// invokeInventoryAction('Medkit', inventory.add);
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
+
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+// invokeInventoryAction('Gas mask', inventory.remove);
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
+
+// console.log(inventory.items); // ['Knife', 'Medkit']
+
+//-------------------II-variant--------------------
 const inventory = {
   items: ['Knife', 'Gas mask'],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
 
-    inventory.items.push(itemName);
+    this.items.push(itemName);
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
-    inventory.items = inventory.items.filter(item => item !== itemName);
+    this.items = this.items.filter(item => item !== itemName);
   },
 };
 
 const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  action(itemName);
+  action.bind(inventory)(itemName);
 };
 
 invokeInventoryAction('Medkit', inventory.add);
